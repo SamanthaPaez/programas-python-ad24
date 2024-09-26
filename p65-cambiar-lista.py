79e8928c41a7bb90f32333f82d901551ac40c84a
SamanthaPaez <36177253@uaz.edu.mx>
2024-09-25 20:52:07 -0600

    Actividad 10

---
+++ p65-cambiar-lista.py
@@ -0,0 +1,20 @@
+# p65-cambiar-lista - Cambiar los elementos de una lista de numeros
+
+import os; os.system("cls")
+
+nums = [10, 9, 8.5, 6.5, 7.5, 6.2, 9.5]
+
+print("Cambiar o modificar los elementos de una lista")
+
+print(type(nums))
+print("La lista   : ", nums, len(nums))
+
+print("Modificar elementos en las posiciones 0 y 1")
+nums[0] = 7
+nums[1] = 7
+print("Resulta en : ",nums)
+
+print("Modificar los elementos en las posiciones de 2 a 5")
+nums[2:5] = [9.0, 9.0, 9,0]
+print("Resulta en : ",nums)
+
---
+++ p66-agregar-lista.py
@@ -0,0 +1,21 @@
+# p66-agregar-lista - Agregar elementos a una lista de numeros
+
+import os; os.system("cls")
+
+nums=[80.3, 12.5, 60.2, 30.4]
+print("Agregar elementos a una lista de numeros")
+
+print("Los numeros: ", nums, len(nums))
+
+print("Agregar dos elementos adicionales")
+nums.append(90)
+nums.append(100)
+print("Queda: ", nums, len(nums))
+
+print("Insertar elementos en la posicion 1")
+nums.insert(1,85.5)
+print("Queda: ", nums, len(nums))
+
+print("Insertar un rango de elementos")
+nums.extend([110,120,130])
+print("Queda: ", nums, len(nums))
